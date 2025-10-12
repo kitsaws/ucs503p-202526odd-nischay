eff581f68e3d2c92e1cde1208d87bec3ca6254bd
kitsaws <nagpalswastik@gmail.com>
2025-10-12 15:17:38 +0530

    Layout.jsx

---
+++ frontend/src/layouts/Layout.jsx
@@ -0,0 +1,28 @@
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import { ToastContainer } from 'react-toastify'
+
+const Layout = ({ className, children }) => {
+  return (
+    <div className='flex flex-col min-h-screen'>
+      <Navbar />
+      <div className={`flex-1 ${className ? className : 'px-20 py-8'}`}>
+        {children}
+      </div>
+      <Footer />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        theme="light"
+      />
+    </div>
+  )
+}
+
+export default Layout
