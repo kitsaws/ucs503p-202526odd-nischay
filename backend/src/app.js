e3cdef85d97e4d37b9842eaddf92ebf96c666d9d
kitsaws <nagpalswastik@gmail.com>
2025-11-07 16:23:49 +0530

    bug fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,6 +26,12 @@ const app = express();
 //   })
 // );
 
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
+
 const isProd = process.env.NODE_ENV === "production";
 app.enable('trust proxy');
 app.use(session({
@@ -44,11 +50,6 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
